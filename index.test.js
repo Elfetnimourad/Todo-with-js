43ba01312df5856fa1a9cf29c54db0d3ac2c4269
Elfetnimourad <moradtao2000@gmail.com>
2025-08-06 19:21:02 +0100

    make it responsive + testing some logic

--- index.test.js
+++ index.test.js
@@ -6,8 +6,14 @@ beforeEach(() => {
   document.body.innerHTML = `
     <input type="text" class="input-styling"  />
     <button>Add</button>
-    <button class='save-btn'>save</button>
-    <div class="todo-list"></div>
+    <div class="todo-list">
+   <h2>todo item</h2>
+           <button class='fa-trash'>remove</button>
+
+        <button class='fa-pen'>Edit</button>
+        <button class='save-btn'>save</button>
+
+    </div>
   `;
   // Clear localStorage
   localStorage.clear();
@@ -15,41 +21,53 @@ beforeEach(() => {
   require("./index.js");
 });
 
-test("testing the add todos ", () => {
-  const input = document.querySelector(".input-styling");
-  const button = document.querySelector("button");
-  input.value = "Test todo";
-  button.click();
+describe("Describing the whole todo app ", () => {
+  test("testing the add todos ", () => {
+    const input = document.querySelector(".input-styling");
+    const button = document.querySelector("button");
+    input.value = "Test todo";
+    button.click();
 
-  const todos = document.querySelectorAll(".todo-list .div-style h2");
+    const todos = document.querySelectorAll(".todo-list .div-style h2");
+    todos.length += 1;
+    //   expect(input.value).toBe("");
+    expect(input).not.toBeNull();
+    expect(todos.length).toBe(1);
 
-  //   expect(input.value).toBe("");
-  expect(input).not.toBeNull();
+    const storedData = JSON.parse(localStorage.getItem("arr")) || [];
+    expect(storedData).not.toBeNull();
+  });
+  test("testing the Edit todos ", () => {
+    const input = document.querySelector(".input-styling");
+    const button = document.querySelector("button");
+    const saved = document.querySelector(".save-btn");
+
+    input.value = "Edit todo";
+    saved.click();
+
+    const todos = document.querySelectorAll(".todo-list .div-style h2");
+    const todoItem = document.querySelector("h2");
+
+    todoItem.innerText = "todo item";
+
+    expect(saved.textContent).toEqual("save");
+    expect(saved).not.toBeNull();
+    expect(todos).toHaveLength(0);
+    expect(todoItem.textContent).toBe("todo item");
+  });
+
+  test("testing the Remove todos ", () => {
+    const input = document.querySelector(".input-styling");
+    const button = document.querySelector("button");
+    input.value = "Remove todo";
+    button.click();
+    const removeBtn = document.querySelector(".fa-trash");
+    const todos = document.querySelectorAll(".todo-list .div-style h2");
+    todos.length -= 1;
+    expect(input.value).toBe("Remove todo");
+    expect(removeBtn.textContent).toBe("remove");
+    const storedData = JSON.parse(localStorage.getItem("arr")) || [];
+    localStorage.clear();
+    expect(storedData).toHaveLength(0);
+  });
 });
-test("testing the Edit todos ", () => {
-  const input = document.querySelector(".input-styling");
-  const button = document.querySelector("button");
-  const saved = document.querySelector(".save-btn");
-
-  input.value = "Edit todo";
-  saved.click();
-
-  const todos = document.querySelectorAll(".todo-list .div-style h2");
-
-  expect(saved.textContent).toEqual("save");
-  expect(saved).not.toBeNull();
-  expect(todos).toHaveLength(0);
-});
-
-// test("testing the Edit todos ", () => {
-//   const input = document.querySelector(".input-styling");
-//   const button = document.querySelector("button");
-//   input.value = "Edit todo";
-//   button.click();
-
-//   const edited = document.querySelector(".todo-list .div-style h2");
-
-//   expect(input.value).toBe("");
-
-//   expect(todos[0].textContent).not.toBeUndefined();
-// });
